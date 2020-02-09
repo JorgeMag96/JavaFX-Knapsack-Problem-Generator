9c8fc3a078cd2e7365fca302c76cf56062c6c41c
Jorge Villarreal <jorge.vi96@gmail.com>
2020-02-09 03:17:32 -0600

    Added number format to fields

--- KnapsackProblemGenerator/src/com/controller/Controller.java
+++ KnapsackProblemGenerator/src/com/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import java.util.function.UnaryOperator;
+
 import com.models.Item;
 import com.models.Knapsack;
 import com.tools.Heuristic;
@@ -15,7 +17,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
 
 public class Controller{
 
@@ -25,6 +30,18 @@ public class Controller{
 	
 	@FXML
     public void initialize() {
+		number_of_instances.setValueFactory(
+	            new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20)
+		        );		
+		
+		UnaryOperator<Change> number = change -> (change.getControlNewText().matches("([1-9][0-9]*)?")) ? change : null;
+		
+		number_of_items.setTextFormatter(new TextFormatter<>(number));
+		min_item_value.setTextFormatter(new TextFormatter<>(number));
+		max_item_value.setTextFormatter(new TextFormatter<>(number));
+		min_item_weight.setTextFormatter(new TextFormatter<>(number));
+		max_item_weight.setTextFormatter(new TextFormatter<>(number));
+		
 		//heuristicType.getItems().addAll(HeuristicType.values());
 		//heuristicType.setValue(HeuristicType.RATIO);
     }
@@ -64,7 +81,27 @@ public class Controller{
     }
 	
 	public void generateInstances() {
-		System.out.println("Clicked");
+		boolean valid = true;
+		if(!min_item_value.getText().isEmpty() || !max_item_value.getText().isEmpty()) {
+			if(Integer.parseInt(min_item_value.getText()) > Integer.parseInt(max_item_value.getText())) {
+				System.out.println("Item max value can't be less than min value");			
+				max_item_value.setText("");
+				valid = false;
+			}
+		}
+		else {
+			System.out.println("Please fill all the fields.");
+			return;
+		}
+		
+		if(Integer.parseInt(min_item_weight.getText()) > Integer.parseInt(max_item_weight.getText())) {
+			System.out.println("Item max weight can't be less than min weight");			
+			max_item_weight.setText("");
+			max_item_weight.requestFocus();
+			valid = false;
+		}
+		
+		if(valid)System.out.println("Everything is valid !");
 	}
 	
 	public Button 		runHeuristic;
@@ -78,4 +115,5 @@ public class Controller{
 	public Slider 		knp_weight_percent;
 	public Spinner<Integer> 		number_of_instances;
 	public ComboBox<HeuristicType> 	heuristicType;
+	
 }
