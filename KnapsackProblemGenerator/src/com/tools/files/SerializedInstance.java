da093535cfcd082bea3f9e43acb0ca0d337b7c58
Jorge Villarreal <jorge.vi96@gmail.com>
2020-02-09 22:53:04 -0600

    Added serialization class

--- KnapsackProblemGenerator/src/com/tools/files/SerializedInstance.java
+++ KnapsackProblemGenerator/src/com/tools/files/SerializedInstance.java
@@ -1,24 +1,55 @@
 package com.tools.files;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.models.Item;
 import com.models.Knapsack;
 
 public class SerializedInstance {
-
-	public SerializedInstance(Knapsack knapsack, Item[] items) {
-		this.knapsack 	= knapsack;
-		this.items 		= items;
-	}
-	
-	public void saveInstance() {
+	//TODO: Here we are going to generate and serialize the problem instance, and save it to the instance folder.
+	public static String saveInstance(Knapsack knapsack, Item[] items) throws IOException{
 		
+		//	Look for the next instance id in the settings .txt	
+		File settingsFile = new File(System.getProperty("user.dir")+"\\configurations\\settings.txt");
+		FileReader fr = new FileReader(settingsFile);
+	    char [] a = new char[50];
+	    fr.read(a);	    
+	    
+	    StringBuilder line = new StringBuilder();
+	    for(char c : a) {
+	    	if(c > 47 && c < 58)
+	    		line.append(c);
+	    }
+	    
+	    int nextVal = Integer.parseInt(line.toString())+1;
+	    fr.close();
+	    
+	    FileWriter writer = new FileWriter(settingsFile);
+		writer.write("instances:"+nextVal);
+		writer.flush();
+		writer.close();
+		
+		//	Create new instance file
+		File file = new File(System.getProperty("user.dir")+"\\instances\\in_"+nextVal+".txt");
+		if(file.createNewFile()) {
+			writer = new FileWriter(file);
+			
+			writer.write("Knapsack weight cap = "+knapsack.WEIGHT_CAP+"\n");
+			writer.write("\nItems:\n");
+			writer.write("i\tw\tv\n");
+			for(int i = 0; i < items.length; i++) {
+				writer.write(i+"\t"+items[i].getWeight()+"\t"+items[i].getValue()+"\n");
+			}			
+			writer.flush();
+			writer.close();
+		}else {
+			throw new IOException("Error while creating file "+file);
+		}
+		
+		return "in_"+nextVal+".txt";
 	}
 	
-	public int getID() {
-		return id;
-	}
-	
-	private int id;
-	private Knapsack knapsack;
-	private Item[] items;
 }
